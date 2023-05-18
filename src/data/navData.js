27c40b31a237c16df5f8bebdfed5dd87f9c64678
Adi pranoto <miuprix@gmail.com>
2023-05-18 07:06:26 +0000

    Dinamic routes

---
+++ src/data/navData.js
@@ -0,0 +1,21 @@
+const navData = [
+    {
+        name: "Home",
+        path: "/",
+    },
+    {
+      name: "About",
+      path: "/about/",
+    },
+    {
+      name: "Blog",
+      path: "/blog/",
+    },
+    {
+        name: "Tags",
+        path: "/tags/",
+    },
+  
+  ]
+  
+  export default navData;
